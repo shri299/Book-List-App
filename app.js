9f10c8ed8ee270474df80d46f39d9e1bae3eb6d4
srishti srivastava <ssrishti508@gmail.com>
2022-01-26 15:59:10 +0530

    Add Book

---
+++ app.js
@@ -0,0 +1,50 @@
+function Book(title,author,isbn)
+{
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+
+
+function UI() {
+
+}
+
+UI.prototype.addBookToList = function (book) {
+    const list = document.getElementById('book-list');
+
+    //create tr element
+    const row = document.createElement('tr');
+    //insert calls
+    row.innerHTML = `<td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a hrf="#" class="delete">X</a></td>`;
+
+    list.appendChild(row);
+}
+
+UI.prototype.clearFields = function () {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+}
+
+//Event listeners
+
+document.getElementById('book-form').addEventListener('submit',function (e) {
+
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const isbn = document.getElementById('isbn').value; 
+    
+    const book = new Book(title,author,isbn);
+    const ui = new UI();
+
+    //add book to list
+    ui.addBookToList(book);
+    //clear field
+    ui.clearFields();
+    e.preventDefault();
+    
+})
